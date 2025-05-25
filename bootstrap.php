ddc476340fc9dfd826740e179f6e476b77476a08
fullStuckDev22 <novikovalexei22@gmail.com>
2025-05-25 21:13:13 +0300

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,10 @@
 <?php
 use AmoCRM\Client\AmoCRMApiClient;
 
-$clientId = '2c3e5ffb-71f6-4363-8f38-e7fc5e589b47';
-$clientSecret = '3zxdVXzNzV3VoWsHAk9uZqxFbaYNqDFP78WqPrHznP40jYccgQK5Pyl4afStIcpm';
-$redirectUri = 'https://testamo.ru/amo/amo.php';
-$baseDomain = 'novikovalexei22.amocrm.ru';
+$clientId = '';
+$clientSecret = '';
+$redirectUri = '';
+$baseDomain = '';
 
 $apiClient = new AmoCRMApiClient($clientId, $clientSecret, $redirectUri);
 
